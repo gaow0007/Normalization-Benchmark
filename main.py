c8bed7d9b0837a7c40d4db82456d3577f0ec8562
SaberArthurus <SaberArthurus@gmail.com>
2018-03-24 11:29:51 +0800

    newop

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 import argparse
 
 from models import *
-from utils import progress_bar
+from utils import *
 from torch.autograd import Variable
 
 
@@ -93,7 +93,7 @@ def main():
         return
     best_prec1 = 0
     for epoch in range(args.start_epoch, args.epochs):
-        def adjust_learning_rate(optimizer=optimizer, epoch=epoch, lr=args.lr, schedule=args.schedule, gamma=args.gamma):
+        adjust_learning_rate(optimizer=optimizer, epoch=epoch, lr=args.lr, schedule=args.schedule, gamma=args.gamma):
 
         train(train_loader, model, criterion, optimizer, epoch)
         prec1 = validate(val_loader, model, criterion)
@@ -209,48 +209,6 @@ def validate(val_loader, model, criterion):
             .format(top1=top1, top5=top5))
 
     return top1.avg
-
-def adjust_learning_rate(optimizer, epoch, lr, schedule, gamma):
-    """Sets the learning rate to the initial LR decayed by schedule"""
-    if epoch in schedule:
-        lr *= gamma
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
-    return lr
-
-
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
-    maxk = max(topk)
-    batch_size = target.size(0)
-
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-    res = []
-    for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-        res.append(correct_k.mul_(100.0 / batch_size))
-    return res
 
 
 if name == '__main__':
